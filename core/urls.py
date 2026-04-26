09f890f67692d7e1a4213967de262da5c75285ed
MrQin877 <kuanchinzhong@gmail.com>
2026-04-26 14:21:05 +0800

    schema docs

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/checkins/', include('checkin.urls')),
     path('api/v1/auth/token/', TokenObtainPairView.as_view()), 
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
